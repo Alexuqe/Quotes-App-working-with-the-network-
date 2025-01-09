4b210d3c7d37b1313d697d63c51d0b04300de95a
alex <hordiah1@google.com>
2025-01-09 21:52:34 +0300

    Initial Commit

---
+++ Quotes App (working with the network)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotes App (working with the network)
+//
+//  Created by Sasha on 9.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
